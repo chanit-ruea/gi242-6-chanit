d8b623b6f135de89b364bccf2c764f8161974c1a
chanit-ruea <chanit.ruea@bumail.net>
2024-03-13 12:37:59 +0700

    Project

---
+++ Assets/Scripts/UI/ActionManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ActionManager : MonoBehaviour
+{
+    public static ActionManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
